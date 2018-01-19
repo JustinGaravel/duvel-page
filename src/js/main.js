d0619489a5c17de7001fbb2714d93c4ec42bc228
Justin Garavel <xxjeep@gmail.com>
2018-01-19 14:18:59 +0900

    javascript scrolls to correct location in sections with smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 console.log('main.js loaded...');
+
+function toTheBeer() {
+  document.querySelector('#the-beer').scrollIntoView({ 
+    behavior: 'smooth' ,
+    block: 'start',
+  });
+};
+
+function toCelebratingMastery() {
+  document.querySelector('#celebrating-mastery').scrollIntoView({ 
+    behavior: 'smooth' ,
+    block: 'start',
+
+  });
+};
+
+function toSince1871() {
+  document.querySelector('#since-1871').scrollIntoView({ 
+    behavior: 'smooth' ,
+    block: 'start',
+
+  });
+};
