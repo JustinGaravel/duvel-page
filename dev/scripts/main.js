f006e62392cfe8a465647450ef9efd91468e1a9d
Justin Garavel <xxjeep@gmail.com>
2018-01-22 09:37:27 +0900

    code cleanup

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,21 +1,21 @@
 
 function toTheBeer() {
   document.querySelector('#the-beer').scrollIntoView({ 
-    behavior: 'smooth' ,
+    behavior: 'smooth',
     block: 'start',
   });
 };
 
 function toCelebratingMastery() {
   document.querySelector('#celebrating-mastery').scrollIntoView({ 
-    behavior: 'smooth' ,
+    behavior: 'smooth',
     block: 'start',
   });
 };
 
 function toSince1871() {
   document.querySelector('#since-1871').scrollIntoView({ 
-    behavior: 'smooth' ,
+    behavior: 'smooth',
     block: 'start',
   });
 };
